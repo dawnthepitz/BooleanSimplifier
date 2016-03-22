e99c721fefa4a0c779a9100dcecf86af771be6c0
Joshua Dapitan <joshuadapitan@gmail.com>
2016-03-22 21:56:16 +0800

    Initial Commit
    Simplifier of Boolean Expression

---
+++ BooleanSimplifier/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSimplify = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtVariable = new System.Windows.Forms.TextBox();
+            this.txtEquation = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSimplify
+            // 
+            this.btnSimplify.Location = new System.Drawing.Point(264, 132);
+            this.btnSimplify.Name = "btnSimplify";
+            this.btnSimplify.Size = new System.Drawing.Size(75, 23);
+            this.btnSimplify.TabIndex = 0;
+            this.btnSimplify.Text = "Simplify";
+            this.btnSimplify.UseVisualStyleBackColor = true;
+            this.btnSimplify.Click += new System.EventHandler(this.btnSimplify_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input Variables:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Input Equation:";
+            // 
+            // txtVariable
+            // 
+            this.txtVariable.Location = new System.Drawing.Point(111, 67);
+            this.txtVariable.Name = "txtVariable";
+            this.txtVariable.Size = new System.Drawing.Size(228, 20);
+            this.txtVariable.TabIndex = 3;
+            // 
+            // txtEquation
+            // 
+            this.txtEquation.Location = new System.Drawing.Point(109, 96);
+            this.txtEquation.Name = "txtEquation";
+            this.txtEquation.Size = new System.Drawing.Size(230, 20);
+            this.txtEquation.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 166);
+            this.Controls.Add(this.txtEquation);
+            this.Controls.Add(this.txtVariable);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSimplify);
+            this.Name = "Form1";
+            this.Text = "Boolean Simplifier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSimplify;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtVariable;
+        private System.Windows.Forms.TextBox txtEquation;
+    }
+}
+
---
+++ BooleanSimplifier/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MLApp;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSimplify_Click(object sender, EventArgs e)
+        {
+            MLApp.MLApp matlab=new MLApp.MLApp();
+            matlab.Visible = 0;
+            MessageBox.Show(matlab.Execute("syms "+txtVariable.Text+";simplify("+txtEquation.Text+");disp(ans)"));
+        }
+    }
+}
